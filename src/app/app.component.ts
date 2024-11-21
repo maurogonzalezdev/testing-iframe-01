1831ee4b1984beff2af30df97102cc9824b73358
Mauricio González <hello@mauro-gonzalez.dev>
2024-11-21 16:05:54 -0300

    final 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, Inject, ViewChild} from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, PLATFORM_ID, ViewChild} from '@angular/core';
 import  {environment} from '../environments/environment';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,28 @@ import  {environment} from '../environments/environment';
   templateUrl: './app.component.html',
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public showPane = false;
   public apiKey = environment.apiKey;
+  public userLevel?: string;
 
 @ViewChild('inputText') inputText?: ElementRef;
 
+constructor(@Inject(PLATFORM_ID) private _platformId: any){}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this._platformId)) {
+      window.addEventListener('message', (event) => {
+        // Verifica el origen del mensaje
+        if (event.origin !== 'https://my-testing-forum-1.forumotion.com/') return;
+
+        if (event.data && event.data.userlevel) {
+          this.userLevel = event.data.userlevel;
+          console.log('User Level:', this.userLevel);
+        }
+      });
+    }
+  }
 
 public togglePane(): void {
   if (this.inputText?.nativeElement.value === this.apiKey) {
