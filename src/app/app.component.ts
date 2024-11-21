9f8d7b9661c2dee3d9c0a6fb46ca8091f6bc4039
Mauricio González <hello@mauro-gonzalez.dev>
2024-11-21 15:02:25 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styles: [],
+})
+export class AppComponent {
+  title = 'project-iframe';
+}
